c7b42fdef38073e2ee277c9cec0581a60b128af2
Pakcy-Tang <lpreterite@126.com>
2019-08-22 14:45:48 +0800

    fix index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 export { default as DetailMixin } from './mixins/Detail.mixin'
-export { default as LeaveBlockMixin } from './mixins/LeaveBlock.mixin'
+export { default as DirtyHandlerMixin } from './mixins/DirtyHandler.mixin'
 export { default as LoaderMixin } from './mixins/Loader.mixin'
 export { default as MessageMixin } from './mixins/Message.mixin'
 export { default as ModalMixin } from './mixins/Modal.mixin'
 export { default as ModelMixin } from './mixins/Model.mixin'
 export { default as RedirectMixin } from './mixins/Redirect.mixin'
 export { default as StatusMixin } from './mixins/Status.mixin'
+export { default as CheckerMixin } from './mixins/Checker.mixin'
 
-// export default {
-//     DetailMixin,
-//     LeaveBlockMixin,
-//     LoaderMixin,
-//     MessageMixin,
-//     ModalMixin,
-//     ModelMixin,
-//     RedirectMixin,
-//     StatusMixin
-// };
+export { default as FilterBar } from './components/FilterBar'
